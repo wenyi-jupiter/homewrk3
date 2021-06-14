984bfec75986037a07937fad8d1237a00cbeb291
wenyi-jupiter <85665185+wenyi-jupiter@users.noreply.github.com>
2021-06-13 23:13:32 -0700

    Add files via upload

---
+++ Exercise5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool debug = false;
+            complexnumber n1 = new complexnumber(5, 2);
+            Console.WriteLine("number is" + n1.tostring());
+
+            n1.setreal(3);
+            Console.WriteLine("number is" + n1.tostring());
+            Console.WriteLine("magnitutude is" + n1.GetMagnitude());
+
+            complexnumber n2 = new complexnumber(-1, 1);
+            n2.Add(n1, n2);
+            Console.WriteLine("after adding");
+            Console.WriteLine(n2.tostring());
+        }
+    }
+}
---
+++ Exercise5/complexnumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise5
+{
+    class complexnumber
+    {
+        private int real;
+        private int img;
+        public complexnumber(int a, int b)
+        {
+            real = a;
+            img = b;
+            
+        }
+        public void setimg(int i) {
+            img = i;
+        }
+
+        public void setreal(int r)
+        {
+            real = r;
+        }
+        public string tostring()
+        {
+            string res = "(" + Convert.ToString(real) + "," + Convert.ToString(img) + ")";
+            return res;
+        }
+
+        public double GetMagnitude()
+        {
+            return Math.Sqrt(Math.Pow(real, 2) + Math.Pow(img, 2));
+        }
+        public void Add(complexnumber n1,complexnumber n2)
+        {
+            real=n1.real+n2.real;
+            img=n1.img+n2.img;
+        }
+
+    }
+}
